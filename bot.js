8c7ad36ce2540cc9823054f192e6a983f922516b
AphoticL <taikoyuup@hotmail.com>
2017-12-15 16:28:13 +0700

    Added main file. Still have a freaking lot to do

---
+++ bot.js
@@ -0,0 +1,19 @@
+let startT = new Date()
+const {Client, RichEmbed} = require('discord.js')
+const irc = require('irc')
+let channel = '#italia'
+const client = new irc.Client('irc.freenode.net', 'Discord2Openkore', {
+    channels: [channel],
+});
+console.log('Application Started')
+
+
+
+client.addListener('message', async (from, to, message) => {
+	// client.say(channel, "I'm a bot!");
+    console.log(from + ' => ' + to + ': ' + message);
+});
+
+client.addListener('error', function(message) {
+    console.log('error: ', message);
+});
